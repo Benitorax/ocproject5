22d07629f69a3be1aea680c20714f20831c33326
Benitorax <sdkyo@hotmail.com>
2021-03-25 15:36:00 +0100

    Edit and refactor FixturesController

--- src/Controller/FixturesController.php
+++ src/Controller/FixturesController.php
@@ -7,59 +7,134 @@ use App\Model\Post;
 use App\Model\User;
 use App\DAO\PostDAO;
 use App\DAO\UserDAO;
+use Framework\View\View;
 use App\Service\IdGenerator;
 use App\Service\PostManager;
 use Framework\Response\Response;
+use Framework\Container\Container;
 use Framework\Controller\AbstractController;
 use Framework\Security\Encoder\PasswordEncoder;
 
 class FixturesController extends AbstractController
 {
+    private $encoder;
+    private $postManager;
+    private $postDAO;
+    private $userDAO;
+    private $faker;
+
+    public function __construct(
+        View $view,
+        Container $container,
+        PasswordEncoder $encoder, 
+        PostManager $postManager, 
+        PostDAO $postDAO, 
+        UserDAO $userDAO
+    )
+    {
+        parent::__construct($view, $container);
+        $this->encoder = $encoder;
+        $this->postManager = $postManager;
+        $this->postDAO = $postDAO;
+        $this->userDAO = $userDAO;
+
+        $this->faker = Factory::create('en_GB');
+    }
+    
+    /**
+     * Loads fixtures
+     */
     public function load(): Response
     {
-        $encoder = $this->get(PasswordEncoder::class);
-        $postManager = $this->get(PostManager::class);
-        $postDAO = $this->get(PostDAO::class);
-        $userDAO = $this->get(UserDAO::class);
-        $faker = Factory::create('en_GB');
+        $this->createUsers(4);
 
         for ($i = 0; $i < 3; $i++) {
-            $firstName = $faker->firstName();
-            $lastName = $faker->lastName;
-            $dateTime = $faker->dateTimeBetween('-2 years', '1 year');
-
-            // create User
-            $user = new User();
-            $user->setId(IdGenerator::generate())
-                ->setEmail(strtolower($firstName . '.' . $lastName) . '@yopmail.com')
-                ->setPassword((string) $encoder->encode('123456'))
-                ->setUsername($firstName . ' ' . $lastName)
-                ->setCreatedAt($dateTime)
-                ->setUpdatedAt($dateTime);
-
-            for ($j = 0; $j < 8; $j++) {
-                $dateTime = $faker->dateTimeBetween('-1 years', 'now');
-
-                // create Post published by the User
-                $post = new Post();
-                $post->setId(IdGenerator::generate())
-                    ->setTitle($faker->realText(70, 5))
-                    ->setSlug($postManager->slugify($post->getTitle()))
-                    ->setLead($faker->realText(255, 3))
-                    ->setContent($faker->paragraphs(3, true))
-                    ->setCreatedAt($dateTime)
-                    ->setUpdatedAt($dateTime)
-                    ->setIsPublished(true)
-                    ->setUser($user);
-
-                $postDAO->add($post);
-            }
-
-            $userDAO->add($user);
+            $user = $this->createAdminUser();
+            $this->createPosts($user, 15);
         }
 
         $this->addFlash('success', 'Fixtures load with success! ');
 
         return $this->redirectToRoute('home');
+    }
+
+    /**
+     * Creates and saves a number of posts.
+     */
+    public function createPosts(User $user, int $numberOfPosts): void 
+    {
+        for ($i = 0; $i < $numberOfPosts; $i++) {
+            $this->createPost($user);
+        }
+    }
+
+
+    /**
+     * Creates and saves a number of users.
+     */
+    public function createUsers(int $numberOfUsers): void 
+    {
+        for ($i = 0; $i < $numberOfUsers; $i++) {
+            $this->createUser();
+        }
+    }
+
+    /**
+     * Returns a created and saved admin user.
+     */
+    public function createAdminUser(): User
+    {
+        return $this->createUser(true);
+    }
+
+    /**
+     * Returns a created and saved user.
+     */
+    public function createUser(bool $isAdmin = false): User
+    {
+        $firstName = $this->faker->firstName();
+        $lastName = $this->faker->lastName;
+        $dateTime = $this->faker->dateTimeBetween('-2 years', '1 year');
+
+        $user = (new User())->setId(IdGenerator::generate())
+            ->setEmail(strtolower($firstName . '.' . $lastName) . '@yopmail.com')
+            ->setPassword((string) $this->encoder->encode('123456'))
+            ->setUsername($firstName . ' ' . $lastName)
+            ->setCreatedAt($dateTime)
+            ->setUpdatedAt($dateTime)
+        ;
+
+        if($isAdmin) {
+            $user->setRoles(['user', 'admin']);
+        }
+
+        $this->userDAO->add($user);
+
+        return $user;
+    }
+
+    /**
+     * Returns a created and saved post.
+     */
+    public function createPost(User $user): Post
+    {
+        $dateTime = $this->faker->dateTimeBetween('-1 years', 'now');
+        $isPublished = random_int(0, 100) > 70;
+
+        $post = new Post();
+        $post->setId(IdGenerator::generate())
+            ->setTitle($this->faker->realText(70, 5))
+            ->setSlug($this->postManager->slugify($post->getTitle()))
+            ->setLead($this->faker->realText(255, 3))
+            ->setContent($this->faker->paragraphs(3, true))
+            ->setCreatedAt($dateTime)
+            ->setUpdatedAt($dateTime)
+            ->setIsPublished($isPublished)
+            ->setUser($user)
+        ;
+
+        $this->postDAO->add($post);
+
+        return $post;
     }
 }
