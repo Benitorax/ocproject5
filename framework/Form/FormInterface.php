ad5c12660ffc99298b8e7471c14c60ab638175c4
Benitorax <sdkyo@hotmail.com>
2021-04-04 15:44:49 +0200

    Add some phpdoc to FormInterface

--- framework/Form/FormInterface.php
+++ framework/Form/FormInterface.php
@@ -18,4 +18,22 @@ interface FormInterface
      * Returns a new instance of the form.
      */
     public function newInstance(): self;
+
+    /**
+     * If your form can be hydrated with a variable you can add this method:
+     * 
+     * public function hydrateForm(Class|Array $variable): self
+     * 
+     * The variable and type-hint can be anything, e.g.:
+     * public function hydrateForm(Post $post): self
+     */
+
+    /**
+     * If you set hydrateForm method you may need this method:
+     * 
+     * public function getData: Class|Array
+     * 
+     * The return type-hint can be anything, e.g.:
+     * public function getData(): Post
+    */
 }
